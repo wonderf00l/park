50393dd809ee2a26eb490bcfe6c9978f813aca94
wonderf00l <maks22036@gmail.com>
2023-03-26 16:22:36 +0300

    Initial commit

---
+++ lib/utils/src/utils.cpp
@@ -0,0 +1,7 @@
+#include "utils.hpp"
+
+#include <iostream>
+
+void print_hello() {
+    std::cout << "Hey" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "utils.hpp"
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+    print_hello();
+    return 0;
+}
